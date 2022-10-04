b58be79b7dacc84b3f93b0a838d85ea8c77b4691
Lucie Nemeckova <lucie.nemeckova@gmail.com>
2022-10-04 19:49:47 +0200

    Kamen/nuzky/papir vstup pres cisla

--- 01/Kamen_Nuzky_Papir.py
+++ 01/Kamen_Nuzky_Papir.py
@@ -1,39 +1,35 @@
 # HRA KÁMEN/NŮŽKY/PAPÍR
-
+print()
 print("Zahrajeme si Kámen - Nůžky - Papír!")
 print()
 
 # přiřazení kámen/nůžky/papír do proměnné pc a její převedení na str
 from random import randrange                           
 pc = randrange(3)
-pc = str(pc)  
+pc = str(pc)
 
 # žádáme vstup od hráče
-hrac = input("Vyber si (kámen/nůžky/papír): ")
+hrac = input('Vyber si: \n kámen = 2 \n nůžky = 1 \n papír = 0 \n tvá volba: ')
 print()
-
-# převede vstup celý na malý
-hrac = str(hrac.lower())
-
-# přiřazení do proměnné člověk 
-if (hrac == 'kámen') or (hrac == 'kamen'):
-    hrac = '2'
-elif (hrac == 'nuzky') or (hrac == 'nůžky'):
-    hrac = '1'
-elif (hrac == 'papír') or (hrac == 'papir'):
-    hrac = '0'
-else:
-    print("Zadej jen kámen/nůžky/papír. Jinak nemůžeme hrát...")
+if hrac > '3':
+    print('Vybírej jen čísla od 0, 1, 2. ')
     quit()
 
-# if-elif, pro vypsání výsledku, dle klasické hry kámen/nůžky/papír
-if pc == hrac:
-    print("Plichta.")
-elif (pc == "2" and hrac == "1") or (pc == "1" and hrac == "0") or (pc == "1" and hrac== "3"):
-    print("Počítač vyhrál.")
-elif (hrac == "2" and pc == "1") or (hrac == "1" and pc == "0") or (hrac == "1" and pc=="3"):
-    print("Vyhrál jsi!")
-else:
-    print("Taková možnost není.")
+print('hráč:', hrac)
+print('pc:', pc)
+print()
+
+if pc > hrac:
+    if pc == '2' and hrac == '0':
+        print('Vyhrál jsi!')
+    else:
+        print('Počítač vyhrál.')
+elif hrac > pc:
+        if hrac == '2' and pc == '0':
+            print('Počítač vyhrál')
+        else:
+            print('Vyhrál jsi!')
+elif pc == hrac:
+    print('Plichta')
 
 print()
