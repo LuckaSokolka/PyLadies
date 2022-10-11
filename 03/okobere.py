2c0cfd062ec428290f5bc3b65e220b951cf2ba32
Lucie Nemeckova <lucie.nemeckova@gmail.com>
2022-10-11 12:05:01 +0200

    OPRAVENO  oko bere, retezec

--- 03/okobere.py
+++ 03/okobere.py
@@ -5,25 +5,50 @@ from random import randrange
 soucet = 0
 odpoved = 'ano'
 
+# Zahájení
+print()
+print('Vitej ve hře OKO BERE')
+print()
+print('Pravidla: Lížeš si kartu dokud tvůj součet není roven 21. Pokud je součet > 21, prohrál jsi.')
+
+# dotaz na první kartu a ošetření, že kód neproběhne, pokud hráč nechce pokračovat
+prvni_karta = input('Můžeme začít? ')
+prvni_karta = prvni_karta.lower()
+
+if prvni_karta == 'ne':
+    print('Tak třeba příště.')
+    quit()
+elif prvni_karta != 'ne' and prvni_karta != 'ano':
+    print('Nevím jak tomu rozumět. Odpovídej pouze ano/ne.')
+    prvni_karta = input('Hrajem? ')
+    prvni_karta = prvni_karta.lower()
+
+    if prvni_karta == 'ne':
+        print('Na shledanou. :)')
+        quit()
+
 # dokud chce hráč přidávat karty, tak funguje smyčka
 while odpoved == 'ano':
     karta = randrange(2,11)
     soucet += karta
 
     if soucet < 21:
-        print(f'Otočil jsi: {karta} a součet je {soucet}')
-        odpoved = input(f'Chceš pokračovat? ')
+        print(f'Otočil jsi {karta} a součet je {soucet}')
+        odpoved = input('Chceš pokračovat? ')
         odpoved = odpoved.lower()
         if odpoved != ('ano') and odpoved != ('ne'):
             odpoved = input('Prosím, odpovídej pouze ano nebo ne. Pokračujeme? ')
     elif soucet == 21: 
-        print(f'Vyhrál jsi! Vytáhl sis: {karta}. Tvůj součet je {soucet}')
+        print(f'Vyhrál jsi! Vytáhl sis {karta}. Tvůj součet je {soucet}')
         break
     elif soucet > 21:
-        print(f'Prohrál jsi. Lízl sis: {karta}. Součet {soucet} > 21.')
+        print(f'Prohrál jsi. Lízl sis {karta}. Součet {soucet} > 21.')
         break
 if odpoved == 'ne':
-    print(f'Škoda. Měl jsi {soucet}. Do 21ti ti chybělo {21 - soucet}')
+    print(f'Měl jsi {soucet}. Do 21 chybělo {21 - soucet}')
+print()
+print('Děkujeme za hru.')
+print()
 
 
 
--- 03/retezec.py
+++ 03/retezec.py
@@ -1,7 +1,17 @@
-# záměna písmene ve slově
+# ZÁMĚNA PÍSMENE VE SLOVĚ
+
+# slovo od uživatele
 retezec = input('Napiš slovo: ')
+
+# zadání pozice od uživatele a ošetření, že délka není kratší než pozice
 pozice = int(input('Na které pozici si přeješ změnu? '))
+while pozice > (len(retezec)) and pozice >= 0:
+    pozice = int(input('Pozice má být v rozmezí počtu písmen ve slově.\nZkus to ještě jednou. Pozice: '))
+
+# znak na výměnu
 znak = input('A co tam napíšeme? :) ')
 
 # složené nové slovo z části původního slova a vložené písmeno mezi části
-print(retezec[:pozice - 1] + znak + retezec[pozice:])
+print(retezec[:(pozice - 1)] + znak + retezec[pozice:])
+
+
