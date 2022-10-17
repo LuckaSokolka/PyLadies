83017966e1a32f1efabb3f5c53eaf5f931b5fe6a
Lucie Nemeckova <lucie.nemeckova@gmail.com>
2022-10-17 15:28:52 +0200

    caesar changes

--- 04/cipher_caesar.py
+++ 04/cipher_caesar.py
@@ -1,24 +1,31 @@
 diacritics = ['á', 'é', 'í', 'ó', 'ú', 'ý', 'č', 'ď', 'ě', 'ň', 'ř', 'š', 'ť', 'ž', 'ů']
-ASCII_numbers = [49, 50, 51, 52, 53, 54, 55, 56, 57, 58]
 
-key = input('key ')
-for znak in key:
-    znak_ASCII = ord(znak)
-    if znak_ASCII not in ASCII_numbers:
-        key = int(input('Zadávej jen celá, kladná čísla. Příklad: 2, 5, 15 \nKey: '))
+key = input('key: ')
+while key.isdigit() is False:
+    try:
+        a = int(key)
+    except ValueError:
+        print('Jako key je možné použít pouze celé číslo.')
+        key = input('key: ')
 
-key = int(key)
+plain_text = input('plain text: ')
 
-if key <= 0:
-    key = int(input('Key můžou být jen celá, kladná čísla. Příklad: 2, 5, 15 \nKey: '))
-
-plain_text = input('plain text ')
 for character in plain_text:
-    if character in diacritics:
-        plain_text = input('Bez čísel a diakritiky. Příklad: les, pes, jedna \nPlain text: ')
-    elif character.isnumeric():
-        plain_text = input('Bez čísel a diakritiky. Příklad: les, pes, jedna \nPlain text: ')
-plain_text = plain_text.lower()
+    if character.isalnum():
+        if character in diacritics:
+            print(character, end="")
+        elif character.isnumeric():
+            ciper = chr(((ord(character)) + int(key) - 48) % 10 + 48)
+            print(ciper, end="")
+        elif character.isupper():
+            ciper = chr((ord(character) + int(key) - 65) % 26 + 65)
+            print(ciper, end="")
+        elif character.islower:
+            ciper = chr(((ord(character)) + int(key) - 97) % 26 + 97)
+            print(ciper, end="")
+    else:
+        print(character, end="")
+print()
 
 '''
 ord(character) -> transfer character to number in ASCII
@@ -28,14 +35,4 @@ ord(character) -> transfer character to number in ASCII
 ... % 26 -> secure that 'Z' will by continue from the beginning of the alphabet,
     alphabet have 26 characters
 ... + 97 -> assign number in ASCII
-... chr -> transfer number to character
-
-'''
-
-for character in plain_text:
-    if character.isalpha():
-        ciper = chr(((ord(character)) + int(key) - 97) % 26 + 97)
-        print(ciper, end="")
-    else:
-        print(character, end="")
-print()
+... chr -> transfer number to character'''
