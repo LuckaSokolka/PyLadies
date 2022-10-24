0d01a46c1a5dd982fb75c2a9c8268f8a33f61d92
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-10-24 13:38:52 +0200

    print like a list

--- 05/zviratka_zip.py
+++ 05/zviratka_zip.py
@@ -1,11 +1,11 @@
 pets = ['had', 'andulka', 'pes', 'kočka', 'králík']
 pets_start_second = []
+pets_sorted = []
 
 for pet in pets:
     pets_start_second.append(pet[1:]) 
-pets_sorted_second = zip(pets_start_second, pets)
-pets_sorted_second = list(pets_sorted_second)
-pets_sorted_second.sort()
-for animal in pets_sorted_second:
-    print(animal[1], end=" ")
-    
+pets_and_pets_second = list(zip(pets_start_second, pets))
+pets_and_pets_second.sort()
+for animal in pets_and_pets_second:
+    pets_sorted.append(animal[1])
+print(pets_sorted)
