8123e3a0d9f975dc0dcf2dff3edeb8c14a2efcf8
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-11-03 09:45:07 +0100

    all working, before tests

--- 06 piškvorky/piskvorky1D.py
+++ 06 piškvorky/piskvorky1D.py
@@ -4,50 +4,49 @@ import time
 # TICK-TACK-TOE
 
 
-def score(area):  # function get list(area) and return result in accordance with area
+def score(area):
+    # function get list(area) and return result in accordance with area
 
     # draw
     if '-' not in area:
-        print('"!"')
-        quit()
+        print('"!" REMÍZA')
     # players won
     elif (3 * player_symbol) in area:
-        print('"X"')
-        quit()
+        print('"X" VYHRÁL')
     # pc won
     elif (3 * pc_symbol) in area:
-        print('"O"')
-        quit()
+        print('"O VYHRÁL"')
     # next play
     else:
         print('"-"')
 
 
-def move(area, position, symbol):  # put symbol into area, get area, position 1 - 19, symbol
+def move(area, position, symbol):
+    # put symbol into area, get area, position 1 - 19, symbol
 
     area = list(area)
     area[position] = symbol
     area = ''.join(area)
     return area
 
-def kontrola(position):
-    position = str(position)
-    while position.isdigit():
-      return position
-    print("zadej číslo")
 
-
-def player_move(area):  # get input(position) from player, test input and return area with symbol
+def player_move(area):
+    # get input(position) from player, test input and return area with symbol
 
     while True:
-        position = int(input(f'Políčko (1 - {len(area)}): '))
-        position = position - 1
-        kontrola(position)
-        while int(position) >= 0 and (int(position) < len(area)):
-            if area[int(position)] != "-":
-                print("políčko je obsazené")
-                break
-            return move(area, position, player_symbol)
+        position = input('Políčko (1 - 20): ')
+        if not position.isdigit():
+            print("bereme jen číslo větší než 0")
+            continue
+        position = int(position) - 1
+        if position > (len(area)):
+            print("políčko je nedostupné")
+            continue
+        if (area[position] != "-"):
+            print("obsazeno")
+            continue
+        return move(area, position, player_symbol)
+
 
 def pc_move(area):  # move for pc, position choose by randrange
 
@@ -63,11 +62,14 @@ def tick_tack_toe_1D(play_area):  # all together in game
         play_area = player_move(play_area)
         print(play_area, "hráč")
         score(play_area)
-        print()
+        if ((3 * player_symbol) in play_area):
+            break
         play_area = pc_move(play_area)
         time.sleep(1)
         print(play_area, "pc")
         score(play_area)
+        if ((3 * pc_symbol) in play_area):
+            break
 
 
 pc_symbol = "O"
