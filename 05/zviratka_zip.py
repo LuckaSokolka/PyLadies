a1abaf40265789d605ad0aa88c3358e5fd0c4d48
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-11-10 00:56:07 +0100

    furt se tam cpe zviratka.zip

--- 05/zviratka_zip.py
+++ 05/zviratka_zip.py
@@ -3,7 +3,7 @@ pets_start_second = []
 pets_sorted = []
 
 for pet in pets:
-    pets_start_second.append(pet[1:]) 
+    pets_start_second.append(pet[1:])
 pets_and_pets_second = list(zip(pets_start_second, pets))
 pets_and_pets_second.sort()
 for animal in pets_and_pets_second:
