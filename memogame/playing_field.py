5a42b13cb44cc5244f2c91730cd58bd614b6e94e
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-11-14 10:31:40 +0100

    přidáno vše

---
+++ memogame/playing_field.py
@@ -0,0 +1,19 @@
+def field(columns, lines):
+
+    memofield = []
+
+    for i in range(columns):
+        temp = []
+        for j in range(lines):
+            temp.append("█")
+        memofield.append(temp)
+    return memofield
+
+def vykresleni_pole():
+    for seznam in field(4,4):
+        print(" ")
+        for prvek in seznam:
+            print(prvek, end=" ")
+        print(end="\n")
+
+vykresleni_pole()
