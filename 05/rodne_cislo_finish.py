92498d3b7f2f11f0cbfd4439d2937c5f74d35a07
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-11-14 20:34:20 +0100

    opraveno modulo, len

--- 05/rodne_cislo_finish.py
+++ 05/rodne_cislo_finish.py
@@ -10,51 +10,41 @@ months_31 = [1, 3, 5, 7, 8, 10, 12]
 
 # YEARS
 years = list(range(86, 100)) + list(range(00, 23))
-only_numbers = []
 
 while True:
 
     ID = input('Napiš rodné číslo: ')
-
-    if ID[6] != "/":
-        print("chybí lomítko nebo je špatný počet čísel")
+    if len(ID) != 11:
+        print("Nesprávná délka rodného čísla.")
         continue
 
-    for number in ID:
-        if number.isdigit():
-            only_numbers.append(number)
-
-    if not len(only_numbers) == 10:
-        print("zadané číslo nemá odpovídající počet znaků")
-        only_numbers.clear()
+    only_numbers = (ID.replace('/', ''))
+    if not only_numbers.isdigit() and ID[6] != "/":
+        print("Nesprávný formát.")
         continue
 
-    modulo_eleven = int(''.join(only_numbers))
-    year_ID = int(''.join(only_numbers[:2]))
-    month_ID = int(''.join(only_numbers[2:4]))
-    day_ID = int(''.join(only_numbers[4:6]))
-    end = int(''.join(only_numbers[6:]))
+    modulo_eleven = int(only_numbers)
+    year_ID = int(only_numbers[:2])
+    month_ID = int(only_numbers[2:4])
+    day_ID = int(only_numbers[4:6])
+    end = int(only_numbers[6:])
 
     if end > 6000:
-        print('rč je nevalidní')
-        only_numbers.clear()
+        print('Rodné číslo je nevalidní')
         continue
 
-    if ((modulo_eleven) % 11 == 0) and year_ID in years:
+    if modulo_eleven % 11 == 0 and (year_ID in years):
 
-        if month_ID > 50:
-            month_ID_all = month_ID - 50
-            month_ID = month_ID_all
-
-        if month_ID == 2 and day_ID <= 29:
+        if month_ID % 50 == 2 and day_ID <= 29:
             print('Zadané rodné číslo je v pořádku')
-            quit()
-        elif (month_ID in months_30) and day_ID <= 30:
-            print('Zadané rodné číslo je v pořádku')
-            quit()
-        elif (month_ID in months_31) and day_ID <= 31:
-            print('Zadané rodné číslo je v pořádku')
-            quit()
-        else:
-            print('Rodné číslo je nevalidní')
             break
+        elif month_ID % 50 in months_30 and day_ID <= 30:
+            print('Zadané rodné číslo je v pořádku')
+            break
+        elif month_ID % 50 in months_31 and day_ID <= 31:
+            print('Zadané rodné číslo je v pořádku')
+            break
+        else:
+            print('Rodné číslo je nevalidní.')
+    else:
+        print('Rodné číslo je neplatné. Zkontrolujte, že je správně zadané.')
