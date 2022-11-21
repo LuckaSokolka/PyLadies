4e0d703ddc6d0bc76acdf3ca2b1e50934bb134d1
Lucie <97535919+LuckaSokolka@users.noreply.github.com>
2022-11-21 18:49:46 +0100

    working version pylady

---
+++ pyladie_oop/main.py
@@ -0,0 +1,28 @@
+from pylady import PyLady
+
+
+eliska = PyLady("Eliška")
+
+eliska.eat()
+eliska.study(2)
+eliska.coffee()
+eliska.meditate()
+eliska.study(1)
+eliska.coffee()
+eliska.meditate()
+eliska.eat()
+eliska.study(2)
+eliska.coffee
+eliska.take_advice()
+eliska.meditate()
+eliska.study(2)
+eliska.coffee()
+eliska.meditate()
+
+
+print(eliska)
+print("knowledge", eliska.knowledge)
+print("energie", eliska.energy)
+print("focus", eliska.focus)
+print()
+eliska.control()
